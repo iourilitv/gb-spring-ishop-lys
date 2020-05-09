68f651f93ec5a1f741117f9c1e41e56c479114b3
iourilitv <iourilitv1965@gmail.com>
2020-05-09 06:13:27 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/spring/ishop/GbSpringIshopLysApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.spring.ishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbSpringIshopLysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbSpringIshopLysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/geekbrains/spring/ishop/GbSpringIshopLysApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.spring.ishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbSpringIshopLysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
