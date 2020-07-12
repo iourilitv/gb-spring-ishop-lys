f234c1fe1f86f4627dd2d3f7564e9113b7e4d41d
iourilitv <iourilitv1965@gmail.com>
2020-07-12 09:14:39 +0300

    Ready.

--- src/main/java/ru/geekbrains/spring/ishop/GbSpringIShopLysApplication.java
+++ src/main/java/ru/geekbrains/spring/ishop/GbSpringIShopLysApplication.java
@@ -2,7 +2,8 @@ package ru.geekbrains.spring.ishop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Final project https://github.com/iourilitv/gb-spring-ishop-lys
+//Selenium Test App https://github.com/iourilitv/less7-hw-selenium-test-app
 @SpringBootApplication
 public class GbSpringIShopLysApplication {
 
